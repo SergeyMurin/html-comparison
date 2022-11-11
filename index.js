7852dd3035d52d5cf152277880885057904b02d5
Sergey <sergeimurin29@gmail.com>
2022-11-11 11:18:57 +0300

    clear data

--- index.js
+++ index.js
@@ -18,27 +18,55 @@ const getBodyData = (htmlData) => {
 }
 
 const dataPreparation = (data) => {
-    const bodyData = getBodyData(data);
-    const htmlRegex = /(<([^>]+)>)/ig;
-    let preparedData = clearData(bodyData);
-    console.log(preparedData)
+    const HTML_OPENED_TAG_REGEX = /((<)\w+(>))/g;
+    const HTML_CLOSED_TAG_REGEX = /((<\/)\w+(>))/g;
 
+    const bodyData = getBodyData(data);
+    let preparedData = clearData(bodyData);
+    //console.log(preparedData)
 }
 
 const clearData = (data) => {
     let cleanData = data.replace(/(\r\n|\n|\r)/gm, "");
     cleanData = cleanData.replace(/(\t)/gm, "");
-    cleanData = cleanData.replace(/\s/g, "");
+    cleanData = cleanData.replace(/>\s+|\s+</g, (m) => {
+        return m.trim();
+    })
+    cleanData = clearScriptTagContent(cleanData);
+    // cleanData = cleanData.replace(/\s/g, "");
+    console.log(cleanData);
     return cleanData;
+}
+
+const clearScriptTagContent = (data) => {
+    const HTML_SCRIPT_TAG = /<script[\s\S]*?>[\s\S]*?<\/script>/ig;
+    const HTML_SCRIPT_OPENED_TAG = /<script[\s\S]*?>/ig;
+    const HTML_SCRIPT_CLOSED_TAG = /<\/script>/ig;
+
+    const allScriptTags = data.match(HTML_SCRIPT_TAG);
+    let newData = data.replace(HTML_SCRIPT_TAG, "");
+
+    for (let scriptTag of allScriptTags) {
+        const openTagStr = scriptTag.match(HTML_SCRIPT_OPENED_TAG);
+        const closeTagStr = "</script>";
+        const openTagParts = scriptTag.split(openTagStr);
+        const closeTagParts = openTagParts[1].split(HTML_SCRIPT_CLOSED_TAG);
+        const content = closeTagParts[0].replace(/\s/g, "").trim();
+        newData = newData + openTagStr + content + closeTagStr;
+    }
+    return newData;
 }
 
 
 const run = async () => {
-    const firstHTMLData = await getHTML(firstUrl);
-    const secondHTMLData = await getHTML(secondUrl);
-    dataPreparation(firstHTMLData);
+    try {
+        const firstHTMLData = await getHTML(firstUrl);
+        //const secondHTMLData = await getHTML(secondUrl);
+        dataPreparation(firstHTMLData);
+    } catch (error) {
+        console.error(error);
+    }
 }
-
 
 
 run().catch(error => console.error(error));
